827505885458aeceb6b596b9be77875037ebfcab
mnasrulk <m.nasrul.k@gmail.com>
2021-10-16 05:36:10 +0700

    commit on 'master'

---
+++ src/Strings.java
@@ -0,0 +1,38 @@
+public final class Strings {
+
+    private Strings(){
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static boolean containsOnlyDigitsV1(String str){
+
+        if (str == null || str.isBlank()){
+            return false;
+        }
+
+        for (int i = 0; i < str.length();i++){
+            if(!Character.isDigit(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean containsOnlyDigitsV2(String str){
+
+        if (str == null || str.isBlank()){
+            return false;
+        }
+
+        return str.matches("[0-9]+");
+    }
+
+    public static boolean containsOnlyDigitsV3(String str){
+
+        if (str == null || str.isBlank()){
+            return false;
+        }
+
+        return !str.chars().anyMatch(n -> !Character.isDigit(n));
+    }
+}
